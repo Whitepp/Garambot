6f25ac67d0895ff9c2625d35ed495b6fe4951cd8
Whitepp <dana_bak@naver.com>
2024-12-21 18:04:01 +0900

    Update testfile.py

--- testfile.py
+++ testfile.py
@@ -38,7 +38,8 @@ async def on_ready():
     await client.change_presence(status=discord.Status.online, activity=game)
 
 async def get_spreadsheet(ws_name):
-    creds = ServiceAccountCredentials.from_json_keyfile_name("google-credentials.json", scope)
+    #creds = ServiceAccountCredentials.from_json_keyfile_name("google-credentials.json", scope)
+    credentials = ServiceAccountCredentials.from_json_keyfile_dict(server/google-credentials.json, scope)
     auth = gspread.authorize(creds)
 
     if creds.access_token_expired:
